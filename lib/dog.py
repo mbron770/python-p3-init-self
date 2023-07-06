0534981453fadad01fb38a628715e4a150482086
mbron770 <mbronfin.tsd@gmail.com>
2023-07-06 00:03:50 -0400

    a

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    
+    def __init__(self, name, breed = 'Mutt'):
+        # print(f'{name} is born!')
+        self.name = name
+        self.breed = breed
+        
+    def bark(self):
+        print('Woof!')
+    # def set_breed(breed = 'Mutt'):
+    #     print(breed)
+        
